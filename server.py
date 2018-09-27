795bff0e69165c1be2f004d3cb31dea32fb543a2
ZIJIAN WANG <jeremyw0001@gmail.com>
2018-09-27 16:04:37 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -9,32 +9,76 @@ app = Flask(__name__)
 # app.config['JSON_SORT_KEYS'] = False
 
 
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-
 @app.route('/', methods=['POST', 'GET'])
-def my_form_post():
-    text = request.form['text']
-    if text is not None:
-        doc = nlp(text)
-        if doc._.has_coref:
-            mentions = [{'start': mention.start_char,
-                         'end': mention.end_char,
-                         'text': mention.text,
-                         'resolved': cluster.main.text
-                         }
-                        for cluster in doc._.coref_clusters
-                        for mention in cluster.mentions]
-            clusters = doc._.coref_clusters
-            resolved = doc._.coref_resolved
-            # return render_template('home.html' + jsonify(mentions))
-            return jsonify(mentions)
-        else:
-            # return render_template('home.html')
-            return doc.text
+def index():
 
+    errors = []
+    results = {}
+
+    if request.method == "POST":
+        text = request.form['text']
+        doc = nlp(text)
+        # mentions = [{'start': mention.start_char,
+        #              'end': mention.end_char,
+        #              'text': mention.text,
+        #              'resolved': cluster.main.text
+        #              }
+        #             for cluster in doc._.coref_clusters
+        #             for mention in cluster.mentions]
+        # clusters = [{'cluster': cluster.main.text
+        #              }
+        #             for cluster in doc._.coref_clusters]
+        #
+        # resolved = doc._.coref_resolved
+        # r = request.get(text)
+        # print(r.text)
+        if doc._.has_coref:
+            # results = [{'start': mention.start_char,
+            #             'end': mention.end_char,
+            #             'text': mention.text,
+            #             'resolved': cluster.main.text
+            #             }
+            #            for cluster in doc._.coref_clusters
+            #            for mention in cluster.mentions]
+            results = [[mention for mention in cluster.mentions]
+                       for cluster in doc._.coref_clusters]
+            return render_template('home.html', errors=errors, results=results)
+        else:
+            errors.append("Query: \""+text+" \"")
+            errors.append("No coreference or input in the query.")
+            return render_template('home.html', errors=errors)
+    else:
+        errors.append("No input in the query.")
+        return render_template('home.html', errors=errors)
+
+
+
+
+
+# @app.route('/', methods=['POST', 'GET'])
+# def my_form_post():
+#     text = request.form['text']
+#     if text is not None:
+#         doc = nlp(text)
+#         if doc._.has_coref:
+#             mentions = [{'start': mention.start_char,
+#                          'end': mention.end_char,
+#                          'text': mention.text,
+#                          'resolved': cluster.main.text
+#                          }
+#                         for cluster in doc._.coref_clusters
+#                         for mention in cluster.mentions]
+#             clusters = [{'cluster': cluster.main.text
+#                          }
+#                         for cluster in doc._.coref_clusters]
+#
+#
+#             resolved = doc._.coref_resolved
+#             return jsonify(mentions)
+#             # return jsonify(clusters)
+#         else:
+#             # return render_template('home.html')
+#             return doc.text
 
 
 @app.route('/about')
